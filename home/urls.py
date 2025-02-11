f1650a4a68ab6c182291ece366e11f09d4f616f5
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2025-02-11 09:49:31 +0000

    add transaction section created

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import login_view, signup_view, logout_view, dashboard_view
+from .views import login_view, signup_view, logout_view, dashboard_view, add_transaction
 
-urlpatterns = [
+urlpatterns =[
     path('', views.home, name='home'),
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path("logout/", logout_view, name="logout"),
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('add_transaction/', add_transaction, name='add_transaction'),  # Added this line
 ]
